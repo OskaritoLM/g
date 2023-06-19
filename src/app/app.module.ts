74cfe5c61863b8616a398cd260b364379feb6fea
Mixcoatl666 <116208536+Mixcoatl666@users.noreply.github.com>
2023-06-18 19:01:10 -0600

    Primera Versión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavarComponent } from './componentes/navar/navar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
