86714a293074d21a76ba975d3f4b77cf049b4d20
Mixcoatl666 <atnoah666@gmail.com>
2023-06-28 09:07:22 -0600

    NavarEliminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavarComponent } from './componentes/navar/navar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
@@ -13,7 +12,6 @@ import { LogadmComponent } from './componentes/logadm/logadm.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavarComponent,
     FooterComponent,
     HomeComponent,
     RegistroComponent,
